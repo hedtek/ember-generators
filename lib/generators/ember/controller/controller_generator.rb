d111cd048366a2e7b16b7699bfca7d6476f193a5
David Workman <workmad3@gmail.com>
2012-03-22 18:05:16 +0000

    Named base provides a name. Don't need to request our own one

--- lib/generators/ember/controller/controller_generator.rb
+++ lib/generators/ember/controller/controller_generator.rb
@@ -1,6 +1,5 @@
 class Ember::ControllerGenerator < Rails::Generators::NamedBase
   source_root File.expand_path("../templates", __FILE__)
-  argument :controller_name, :type => :string
 
   def generate_controller
     template 'controller.js.coffee', "app/assets/javascripts/ember/controllers/#{controller_name}.js.coffee"
@@ -9,5 +8,9 @@ class Ember::ControllerGenerator < Rails::Generators::NamedBase
   private
   def application_name
     Rails.application.class.name.split('::').first
+  end
+
+  def controller_name
+    file_name.downcase
   end
 end
