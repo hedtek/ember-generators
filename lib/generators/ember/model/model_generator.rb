9904fdfe8e95969ff6fd0f49a873640e1e03bc2c
David Workman <workmad3@gmail.com>
2012-03-22 19:20:18 +0000

    Getting to grips with how k:v attributes work with Thor

--- lib/generators/ember/model/model_generator.rb
+++ lib/generators/ember/model/model_generator.rb
@@ -1,6 +1,6 @@
 class Ember::ModelGenerator < Rails::Generators::NamedBase
   source_root File.expand_path("../templates", __FILE__)
-  argument :attributes, :type => :array, :default => [], :banner => "field[:type] field[:type]"
+  argument :schema, :type => :array, :default => [], :banner => "field[:type] field[:type]"
 
   def generate_model
     template 'model.js.coffee.erb', "app/assets/javascripts/ember/models/#{model_name}.js.coffee"
@@ -17,9 +17,5 @@ class Ember::ModelGenerator < Rails::Generators::NamedBase
 
   def route_name
     "#{model_name.pluralize}_path"
-  end
-
-  def schema
-    attributes.map{|a| a.to_s.split(":") << "String" }.reduce({}){|hsh, atr| hsh[atr[0]] = atr[1]; hsh}
   end
 end
